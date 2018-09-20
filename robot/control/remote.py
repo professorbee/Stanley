4f175ce6d0382bd8822854d664a2cf7538a93bdb
Noskcaj <kcajdev@gmail.com>
2018-09-19 20:01:07 -0400

    Add alternate drive mode

--- robot/control/remote.py
+++ robot/control/remote.py
@@ -1,3 +1,40 @@
+# from networktables.util import ntproperty
+
+# import components
+
+
+# class Remote:
+#     """
+#         Allow driving the robot over network tables
+#         Pulls control values from the following keys in the "/RemoteControl" table:
+#             * left_speed: Forward/reverse speed on the left motors
+#             * right_speed: Forward/reverse speed on the right motors
+#             * rotation: Inplace and moving rotation speed
+#             * grab: A True/False of whether or not to close the grabbers (defaults to `True`)
+#     """
+
+#     left_speed = ntproperty("/RemoteControl/left_speed", 0.0)
+#     right_speed = ntproperty("/RemoteControl/right_speed", 0.0)
+#     rotation = ntproperty("/RemoteControl/rotation", 0.0)
+#     grab = ntproperty("/RemoteControl/grab", True)
+
+#     drive: components.drive.Drive
+#     lift: components.lift.Lift
+#     intake: components.intake.Intake
+#     grabber: components.grabber.Grabber
+
+#     def process(self):
+#         if self.grab:
+#             self.grabber.grab()
+#         else:
+#             self.grabber.release()
+
+#         self.drive.direct_drive = True
+#         self.drive.drive_train.tankDrive(-self.left_speed, -self.right_speed)
+
+#     def execute(self):
+#         pass
+
 from networktables.util import ntproperty
 
 import components
@@ -13,8 +50,9 @@ class Remote:
             * grab: A True/False of whether or not to close the grabbers (defaults to `True`)
     """
 
-    left_speed = ntproperty("/RemoteControl/left_speed", 0.0)
-    right_speed = ntproperty("/RemoteControl/right_speed", 0.0)
+    speed = ntproperty("/RemoteControl/speed", 0.0)
+    # left_speed = ntproperty("/RemoteControl/left_speed", 0.0)
+    # right_speed = ntproperty("/RemoteControl/right_speed", 0.0)
     rotation = ntproperty("/RemoteControl/rotation", 0.0)
     grab = ntproperty("/RemoteControl/grab", True)
 
@@ -29,8 +67,9 @@ class Remote:
         else:
             self.grabber.release()
 
-        self.drive.direct_drive = True
-        self.drive.drive_train.tankDrive(-self.left_speed, -self.right_speed)
+        # self.drive.direct_drive = True
+        # self.drive.drive_train.tankDrive(-self.left_speed, -self.right_speed)
+        self.drive.drive(-self.speed, -self.rotation)
 
     def execute(self):
         pass
